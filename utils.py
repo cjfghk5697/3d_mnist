539bcd0c5580b0df7d5e8b5f657b6cc7e64bd3d4
root <cjfghk5697@gmail.com>
2022-08-02 10:45:05 +0000

    first

---
+++ utils.py
@@ -0,0 +1,295 @@
+# augementation 모음집(made by 한철화)
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import math
+from torch.optim.lr_scheduler import _LRScheduler
+import pandas as pd
+import cv2
+from tqdm import tqdm
+import zipfile
+import os
+from PIL import Image
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as T
+from torch.utils.data import DataLoader, Dataset
+
+
+""" 사용 예시 BETA=1.0
+      if BETA > 0 and np.random.random()>0.5: # cutmix 작동될 확률      
+        lam = np.random.beta(BETA, BETA)
+        rand_index = torch.randperm(img.size()[0]).to(device)
+        target_a = label
+        target_b = label[rand_index]            
+        bbx1, bby1, bbx2, bby2 = utils.rand_bbox(img.size(), lam)
+        img[:, :, bbx1:bbx2, bby1:bby2] = img[rand_index, :, bbx1:bbx2, bby1:bby2]
+        lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (img.size()[-1] * img.size()[-2]))
+        output = model(img,csv_feature)
+        loss = criterion(output, target_a) * lam + criterion(output, target_b) * (1. - lam)
+"""
+def rand_bbox(size, lam): # size : [B, C, W, H]
+    W = size[2] # 이미지의 width
+    H = size[3] # 이미지의 height
+    cut_rat = np.sqrt(1. - lam)  # 패치 크기의 비율 정하기
+    cut_w = np.int(W * cut_rat)  # 패치의 너비
+    cut_h = np.int(H * cut_rat)  # 패치의 높이
+
+    # uniform
+    # 기존 이미지의 크기에서 랜덤하게 값을 가져옵니다.(중간 좌표 추출)
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    # 패치 부분에 대한 좌표값을 추출합니다.
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+  
+"""
+사용 예시
+    for batch in (train_loader):
+        optimizer.zero_grad()
+        x = torch.tensor(batch[0], dtype=torch.float32, device=device)
+        y = torch.tensor(batch[1], dtype=torch.long, device=device)
+
+        x, targets_a, targets_b, lam = mixup_data(x, y)
+        x, targets_a, targets_b = map(Variable, (x, targets_a, targets_b))
+
+        #outputs = model(x)
+
+        with torch.cuda.amp.autocast():
+            pred = model(x)
+        loss = mixup_criterion(criterion, pred, targets_a, targets_b, lam)
+
+"""
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+def show_plot(x,y,xlabel,ylabel,title,plot_label_1,plot_label_2):
+  plt.grid()
+  plt.plot(x, label=plot_label_1)
+  plt.plot(y, label=plot_label_2)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title, fontsize=25)
+  plt.legend()
+  plt.show()
+
+
+  ### https://github.com/davda54/sam
+
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, **kwargs)
+        super(SAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+
+"""
+학습 단계에서는 두 가지 부분을 수정해주셔야 합니다.
+
+optimizer 정의
+base optimizer 를 정의한 후 그 base optimizer 를 SAM으로 감싸줍니다.
+loss 계산 부분
+SAM Optimizer 는 앞서 언급했듯이, loss 의 최적화와 loss 의 Sharpness 최적화가 이루어 지기 때문에 loss 계산이 두 번 진행됩니다.
+first_step 과 second_step 함수가 그 역할을 하며, 그래디언트 초기화 옵션(zero_grad=True) 를 주어 계산이 끝나고 weights 가 초기화 될 수 있도록 합니다.
+이 두가지만 수정해주면 다른 부분은 별다른 수정 없이 SAM Optimizer 를 사용해볼 수 있습니다.
+"""
+
+"""
+train 전 수정 사항
+
+
+    base_optimizer = torch.optim.SGD  
+    optimizer = SAM(model.parameters(), base_optimizer, lr=0.001, momentum=0.9)
+                    loss = criterion(probs, labels)
+                    # 중간 노드의 gradient로
+                    # backpropagation을 적용하여
+                    # gradient 계산
+                    loss.backward()
+                    
+                    
+                    # sam optimizer first_steop
+                    optimizer.first_step(zero_grad=True)
+                    
+                    # sam optimizer second_steop
+                    criterion(model(images), labels).backward()
+                    optimizer.second_step(zero_grad=True)
+"""
+
+
+class CosineAnnealingWarmUpRestarts(_LRScheduler):
+    def __init__(self, optimizer, T_0, T_mult=1, eta_max=0.1, T_up=0, gamma=1., last_epoch=-1):
+        if T_0 <= 0 or not isinstance(T_0, int):
+            raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
+        if T_mult < 1 or not isinstance(T_mult, int):
+            raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
+        if T_up < 0 or not isinstance(T_up, int):
+            raise ValueError("Expected positive integer T_up, but got {}".format(T_up))
+        self.T_0 = T_0
+        self.T_mult = T_mult
+        self.base_eta_max = eta_max
+        self.eta_max = eta_max
+        self.T_up = T_up
+        self.T_i = T_0
+        self.gamma = gamma
+        self.cycle = 0
+        self.T_cur = last_epoch
+        super(CosineAnnealingWarmUpRestarts, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if self.T_cur == -1:
+            return self.base_lrs
+        elif self.T_cur < self.T_up:
+            return [(self.eta_max - base_lr)*self.T_cur / self.T_up + base_lr for base_lr in self.base_lrs]
+        else:
+            return [base_lr + (self.eta_max - base_lr) * (1 + math.cos(math.pi * (self.T_cur-self.T_up) / (self.T_i - self.T_up))) / 2
+                    for base_lr in self.base_lrs]
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.cycle += 1
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = (self.T_i - self.T_up) * self.T_mult + self.T_up
+        else:
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                    self.cycle = epoch // self.T_0
+                else:
+                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                    self.cycle = n
+                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+                
+        self.eta_max = self.base_eta_max * (self.gamma**self.cycle)
+        self.last_epoch = math.floor(epoch)
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+            
+            
+class EarlyStopping:
+    """주어진 patience 이후로 validation loss가 개선되지 않으면 학습을 조기 중지"""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): validation loss가 개선된 후 기다리는 기간
+                            Default: 7
+            verbose (bool): True일 경우 각 validation loss의 개선 사항 메세지 출력
+                            Default: False
+            delta (float): 개선되었다고 인정되는 monitered quantity의 최소 변화
+                            Default: 0
+            path (str): checkpoint저장 경로
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''validation loss가 감소하면 모델을 저장한다.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
